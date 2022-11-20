14036779abf16ca933e43590ae270f4baf46238e
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-11-20 18:53:47 +0100

    Add WebJob example using .NET Framework 4.8

---
+++ WebJob1/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob48;
+
+public class QueueFunction
+{
+    private readonly ILogger<QueueFunction> _logger;
+
+    public QueueFunction(ILogger<QueueFunction> logger)
+    {
+        _logger = logger;
+    }
+
+
+    public Task ProcessQueueMessageAsync([QueueTrigger("abc")] string message, TextWriter log)
+    {
+        log.WriteLine(message);
+        _logger.LogCritical("hello : " + message);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ WebJob1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob48;
+
+internal class Program
+{
+    static async Task Main()
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "local";
+
+        var builder = new HostBuilder();
+
+        builder.ConfigureAppConfiguration(cb =>
+        {
+            cb.AddJsonFile("appsettings.json");
+            cb.AddJsonFile($"appsettings.{environment}.json", true, true);
+        });
+
+        builder.ConfigureLogging((hostBuilderContext, loggingBuilder) =>
+        {
+            loggingBuilder.AddConfiguration(hostBuilderContext.Configuration.GetSection("Logging"));
+            loggingBuilder.AddConsole();
+        });
+
+        builder.ConfigureWebJobs(b =>
+        {
+            b.AddAzureStorageCoreServices();
+            b.AddAzureStorageBlobs();
+            b.AddAzureStorageQueues();
+        });
+        
+        var host = builder.Build();
+        using (host)
+        {
+            await host.RunAsync();
+        }
+    }
+}
